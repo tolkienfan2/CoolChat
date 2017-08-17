e9993f73f23c7ce72391f8b00a0389a34f935e7e
Minni K Ang <minni.ang@gmail.com>
2017-08-17 15:09:09 -0400

    added revealVC

---
+++ CoolChat/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  CoolChat
+//
+//  Created by Minni K Ang on 2017-08-17.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+
+    }
+
+
+}
---
+++ CoolChat/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  CoolChat
+//
+//  Created by Minni K Ang on 2017-08-17.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+
+
+}
