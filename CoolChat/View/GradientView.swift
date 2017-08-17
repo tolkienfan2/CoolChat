16b6b0bafebc41c41c4837dc798b44c7444464e6
Minni K Ang <minni.ang@gmail.com>
2017-08-17 15:36:59 -0400

    added gradientView

---
+++ CoolChat/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  CoolChat
+//
+//  Created by Minni K Ang on 2017-08-17.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
