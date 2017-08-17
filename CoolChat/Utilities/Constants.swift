6489f956e5e6a5c54c71704a61c80c9f2eed026c
Minni K Ang <minni.ang@gmail.com>
2017-08-17 19:10:06 -0400

    UI updated

---
+++ CoolChat/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CoolChat
+//
+//  Created by Minni K Ang on 2017-08-17.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
