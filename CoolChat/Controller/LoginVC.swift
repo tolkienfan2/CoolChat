38792d4064c4829890519456a9334fdd9a8c5d07
Minni K Ang <minni.ang@gmail.com>
2017-08-17 20:00:38 -0400

    login screen added

---
+++ CoolChat/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  CoolChat
+//
+//  Created by Minni K Ang on 2017-08-17.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
